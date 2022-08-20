a6b4e836deca45b904cc5f1273a1ac644f24719b
nandometzger <metzgern@ethz.ch>
2022-08-20 13:18:12 +0200

    upload code

--- implicitlayer_cifar_DEQ.py
+++ implicitlayer_cifar_DEQ.py
@@ -44,6 +44,7 @@ def anderson(f, x0, m=5, lam=1e-4, max_iter=50, tol=1e-2, beta = 1.0):
     y[:,0] = 1
     
     res = []
+    Xall = []
     for k in range(2, max_iter):
         n = min(k, m)
         G = F[:,:n]-X[:,:n]
@@ -54,9 +55,10 @@ def anderson(f, x0, m=5, lam=1e-4, max_iter=50, tol=1e-2, beta = 1.0):
         X[:,k%m] = beta * (alpha[:,None] @ F[:,:n])[:,0] + (1-beta)*(alpha[:,None] @ X[:,:n])[:,0]
         F[:,k%m] = f(X[:,k%m].view_as(x0)).view(bsz, -1)
         res.append((F[:,k%m] - X[:,k%m]).norm().item()/(1e-5 + F[:,k%m].norm().item()))
+        Xall.append(X[:,k%m].view_as(x0))
         if (res[-1] < tol):
             break
-    return X[:,k%m].view_as(x0), res
+    return X[:,k%m].view_as(x0), res, torch.cat(Xall,0)
 
 
 class DEQFixedPoint(torch.nn.Module):
@@ -66,36 +68,37 @@ class DEQFixedPoint(torch.nn.Module):
         self.solver = solver
         self.kwargs = kwargs
         
-    def forward(self, x):
+    def forward(self, x, train=True):
         # compute forward pass and re-engage autograd tape
         with torch.no_grad():
-            z, self.forward_res = self.solver(lambda z : self.f(z, x), torch.zeros_like(x), **self.kwargs)
+            z, self.forward_res, zall = self.solver(lambda z : self.f(z, x), torch.zeros_like(x), **self.kwargs)
         z = self.f(z,x)
         
         # set up Jacobian vector product (without additional forward calls)
+        z0 = zall.clone().detach().requires_grad_()
         z0 = z.clone().detach().requires_grad_()
         f0 = self.f(z0,x)
         def backward_hook(grad):
-            g, self.backward_res = self.solver(lambda y : autograd.grad(f0, z0, y, retain_graph=True)[0] + grad,
+            g, self.backward_res,zall = self.solver(lambda y : autograd.grad(f0, z0, y, retain_graph=True)[0] + grad,
                                                grad, **self.kwargs)
             return g
                 
         z.register_hook(backward_hook)
 
-        meta = {
-            "forward_res": self.forward_res, 
-        }
-        return z
+        # meta = {
+        #     "forward_res": self.forward_res, 
+        # }
+        return z, zall
 
 # run a very small network with double precision, iterating to high precision
 f = ResNetLayer(2,2, num_groups=2).double()
-deq = DEQFixedPoint(f, anderson, tol=1e-10, max_iter=500).double()
+deq = DEQFixedPoint(f, anderson, tol=1e-10, max_iter=50).double()
 gradcheck(deq, torch.randn(1,2,3,3).double().requires_grad_(), eps=1e-5, atol=1e-3, check_undefined_grad=False)
 
 f = ResNetLayer(64,128)
-deq = DEQFixedPoint(f, anderson, tol=1e-4, max_iter=100, beta=2.0)
+deq = DEQFixedPoint(f, anderson, tol=1e-4, max_iter=50, beta=2.0)
 X = torch.randn(10,64,32,32)
-out = deq(X)
+out = deq(X)[0]
 (out*torch.randn_like(out)).sum().backward()
 
 
@@ -107,31 +110,56 @@ out = deq(X)
 # plt.ylabel("Residual")
 
 
+class TinyModel(torch.nn.Module):
+
+    def __init__(self, f, chan):
+        super(TinyModel, self).__init__()
+
+        self.pre = model = torch.nn.Sequential(torch.nn.Conv2d(3,chan, kernel_size=3, bias=True, padding=1), torch.nn.BatchNorm2d(chan))
+        self.deq = DEQFixedPoint(f, anderson, tol=1e-2, max_iter=25, m=5)
+        self.post = torch.nn.Sequential( torch.nn.ReLU(), torch.nn.BatchNorm2d(chan),
+                      torch.nn.AvgPool2d(8,8), torch.nn.Flatten(),
+                      torch.nn.Linear(chan*4*4,10))
+    def forward(self, x, train=True):
+        inshape = x.shape
+        x = self.pre(x)
+        if train:
+            x = self.deq(x)[1]
+            x = self.post(x)
+            return x
+        else:
+            x = self.deq(x)[0]
+            x = self.post(x)
+            return x
 torch.manual_seed(0)
 chan = 24
 f = ResNetLayer(chan, 64, kernel_size=3)
-model = torch.nn.Sequential(torch.nn.Conv2d(3,chan, kernel_size=3, bias=True, padding=1),
-                      torch.nn.BatchNorm2d(chan),
-                      DEQFixedPoint(f, anderson, tol=1e-2, max_iter=25, m=5),
-                    #   torch.nn.ReLU(),
-                    #   torch.nn.Conv2d(chan,64, kernel_size=3, bias=True, padding=1),
-                    #   torch.nn.ReLU(),
-                    #   torch.nn.Conv2d(64,chan, kernel_size=3, bias=True, padding=1),
-                      torch.nn.ReLU(),
-                      torch.nn.BatchNorm2d(chan),
-                      torch.nn.AvgPool2d(8,8),
-                      torch.nn.Flatten(),
-                      torch.nn.Linear(chan*4*4,10)).to(device)
+# model = torch.nn.Sequential(torch.nn.Conv2d(3,chan, kernel_size=3, bias=True, padding=1),
+#                       torch.nn.BatchNorm2d(chan),
+#                       DEQFixedPoint(f, anderson, tol=1e-2, max_iter=25, m=5),
+#                     #   torch.nn.ReLU(),
+#                     #   torch.nn.Conv2d(chan,64, kernel_size=3, bias=True, padding=1),
+#                     #   torch.nn.ReLU(),
+#                     #   torch.nn.Conv2d(64,chan, kernel_size=3, bias=True, padding=1),
+#                       torch.nn.ReLU(),
+#                       torch.nn.BatchNorm2d(chan),
+#                       torch.nn.AvgPool2d(8,8),
+#                       torch.nn.Flatten(),
+#                       torch.nn.Linear(chan*4*4,10)).to(device)
 
+model = TinyModel(f,chan).to(device) 
 
 # standard training or evaluation loop
-def epoch(loader, model, opt=None, lr_scheduler=None):
+def epoch(loader, model, opt=None, lr_scheduler=None, train=True):
     total_loss, total_err, total_iter, final_res = 0.,0.,0.,0.
     model.eval() if opt is None else model.train()
     for X,y in tqdm(loader):
         X,y = X.to(device), y.to(device)
-        yp = model(X)
-        loss = nn.CrossEntropyLoss()(yp,y)
+        yp = model(X, train=True)
+        if train:
+            loss = nn.CrossEntropyLoss()(yp,y.repeat(yp.shape[0]//y.shape[0]))
+        else:
+            loss = nn.CrossEntropyLoss()(yp,y)
         if opt:
             opt.zero_grad()
             loss.backward()
@@ -148,8 +176,8 @@ def epoch(loader, model, opt=None, lr_scheduler=None):
 
 cifar10_train = datasets.CIFAR10(".", train=True, download=True, transform=transforms.ToTensor())
 cifar10_test = datasets.CIFAR10(".", train=False, download=True, transform=transforms.ToTensor())
-train_loader = DataLoader(cifar10_train, batch_size = 25, shuffle=True, num_workers=0)
-test_loader = DataLoader(cifar10_test, batch_size = 25, shuffle=False, num_workers=0)
+train_loader = DataLoader(cifar10_train, batch_size = 40, shuffle=True, num_workers=0)
+test_loader = DataLoader(cifar10_test, batch_size = 40, shuffle=False, num_workers=0)
 
 
 if __name__ == '__main__':
@@ -161,6 +189,6 @@ if __name__ == '__main__':
     scheduler = optim.lr_scheduler.CosineAnnealingLR(opt, max_epochs*len(train_loader), eta_min=1e-6)
 
     for i in (range(50)):
-        print(epoch(train_loader, model, opt, scheduler))
+        print(epoch(train_loader, model, opt, scheduler, train=True))
         print(epoch(test_loader, model)) 
 
